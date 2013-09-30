240987eb156503032a296cc5ce43929701be6ea0
troyth <troyth@gmail.com>
2013-09-29 22:24:41 -0400

    %06d

--- eg/timelapse.js
+++ eg/timelapse.js
@@ -3,7 +3,7 @@ var RaspiCam = require("../lib/raspicam");
 
 var camera = new RaspiCam({
 	mode: "timelapse",
-	output: "./image_%6d.jpg", // image_000001.jpg, image_000002.jpg,...
+	output: "./image_%06d.jpg", // image_000001.jpg, image_000002.jpg,...
 	timelapse: 3000, // take a picture every 3 seconds
 	timeout: 12000 // take a total of 4 pictures over 12 seconds
 });
