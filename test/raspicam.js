6cc4d0351aab7c204255516ee1b12fc33116dcb3
troyth <troyth@gmail.com>
2013-09-30 00:04:05 -0400

    pruned grunt unit test

--- test/raspicam.js
+++ test/raspicam.js
@@ -6,28 +6,12 @@ var photo = new RaspiCam({
 	w: 180
 });
 
-var timelapse = new RaspiCam({
-	mode: "timelapse",
-	output: "./image_%6d.jpg", // image_000001.jpg, image_000002.jpg,...
-	timelapse: 3000, // take a picture every 3 seconds
-	timeout: 12000 // take a total of 4 pictures over 12 seconds
-});
 
 
 exports["static"] = {
 	"RaspiCam opts hash": function( test ){
 		test.expect(1);
 		test.equal( photo.get("width"), 180, "photo.width === photo.w");
-		test.done();
-	},
-	"RaspiCam photo start": function( test ){
-		test.expect(1);
-		test.ok( photo.start(), "photo.start()");
-		test.done();
-	},
-	"RaspiCam timelapse start": function( test ){
-		test.expect(1);
-		test.ok( timelapse.start(), "timelapse.start()");
 		test.done();
 	}
 
